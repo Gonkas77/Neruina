19c78ea8502459761e7c76a71727e21cfa63a4d0
Bawnorton <bawnorton@me.com>
2024-09-21 20:43:10 -0700

    drop forge

--- buildSrc/src/main/kotlin/LoaderData.kt
+++ buildSrc/src/main/kotlin/LoaderData.kt
@@ -1,14 +1,13 @@
 import org.gradle.api.Project
 
 class LoaderData(private val project: Project, private val name: String) {
-    val isForge = name == "forge"
     val isFabric = name == "fabric"
     val isNeoForge = name == "neoforge"
 
     fun getVersion() : String = when {
         isNeoForge -> project.property("neoforge_loader").toString()
-        isForge -> project.property("forge_loader").toString()
-        else -> project.property("fabric_loader").toString()
+        isFabric -> project.property("fabric_loader").toString()
+        else -> throw IllegalStateException("$name is not supported")
     }
 
     override fun toString(): String {
